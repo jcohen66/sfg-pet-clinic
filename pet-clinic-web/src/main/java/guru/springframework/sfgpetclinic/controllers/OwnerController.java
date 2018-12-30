3abc3a24cf2f6b71cbdea0dd5fae420adf78749f
jonathan <jcohen66@optonline.net>
2018-12-30 14:45:01 -0500

    List all Owners on Owners Index Page.  Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,29 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        // Set an attribute in the model for owners
+        // The model with the attribute will be referenced
+        // inside the thymeleaf page specified below.
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
