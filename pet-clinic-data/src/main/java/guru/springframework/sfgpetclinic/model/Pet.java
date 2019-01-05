9b410aa8770903e6715b70a0cbac34f926c64360
jcohen66 <jcohen66@optonline.net>
2019-01-05 14:33:47 -0500

    Fixed typo in Pets.  Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -13,7 +13,7 @@ public class Pet extends BaseEntity {
     private PetType petType;
 
     @ManyToOne
-    @JoinColumn name = "owner_id"
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     @Column(name = "birth_date")
