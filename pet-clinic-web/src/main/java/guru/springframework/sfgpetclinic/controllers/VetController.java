2539f8a2ede4cdc48902a5052a7ce4ab9f7805fd
jcohen66 <jcohen66@optonline.net>
2019-01-25 23:02:21 -0500

    Added vets api endpoint  Closes #65

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,32 +1,38 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Set;
+
+/**
+ * Created by jt on 7/22/18.
+ */
 @Controller
-@RequestMapping({"/vets"})
 public class VetController {
 
     private final VetService vetService;
 
-    @Autowired
     public VetController(VetService vetService) {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html","/vets/vets.html"})
-    public String listVets(Model model) {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
 
-    @RequestMapping("/find")
-    public String findVetss() {
-        return "notImplemented";
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
